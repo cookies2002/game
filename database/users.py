2b894619f93ee98a44969ef11cbc788b61ae579e
cookies2002 <cookies2002boy@gmail.com>
2025-07-20 12:37:45 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -6,4 +6,13 @@ async def get_top_players(limit=10):
         ("coins", -1)
     ]).limit(limit).to_list(length=limit)
     return players
+
+async def get_group_top_players(chat_id: int, limit=10):
+    return await users_collection.find({
+        "group_id": chat_id
+    }).sort([
+        ("level", -1),
+        ("xp", -1)
+    ]).limit(limit).to_list(length=limit)
+    
   
