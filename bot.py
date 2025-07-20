9a23181b087b27982b116be6b961438b3cc43bba
cookies2002 <cookies2002boy@gmail.com>
2025-07-20 17:20:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URL = os.getenv("MONGO_URL")
 
 bot = Client("fairy_power_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
