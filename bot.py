9ff4a3f140defe8353c67a6521ab0892613e0c61
cookies2002 <cookies2002boy@gmail.com>
2025-07-22 14:23:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,10 @@ import os
 import asyncio
 import random
 from pyrogram import Client, filters
-from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, InputPeerUser
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from pyrogram.enums import ParseMode
-from pyrogram.types import InputPeerUser
 
 load_dotenv()
 
